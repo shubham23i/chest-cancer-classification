1bea2b5ae155f41b12b6224e1cd0d11c620e7c93
Shubham Joshi <indsjos@gmail.com>
2026-04-16 13:16:31 +0530

    req and logger

---
+++ src/cnnClassifier/__init__.py
@@ -0,0 +1,18 @@
+import os
+import logging
+import sys
+
+logging_str="[%(asctime)s]:[%(levelname)s]: %(module)s: %(message)s"
+
+log_dir="logs"
+log_file_path=os.path.join(log_dir,"running_logs.logs")
+os.makedirs(log_dir,exist_ok=True)
+
+logging.basicConfig(level=logging.INFO,
+                    format=logging_str,
+                    handlers=[
+                        logging.FileHandler(log_file_path),
+                        logging.StreamHandler(sys.stdout)
+                    ]
+)
+logger=logging.getLogger("cnnClassifierLogger")
