e6f35b538490f4cfe58f6674a9824353117badd3
Shubham Joshi <indsjos@gmail.com>
2026-05-09 16:13:41 +0530

    ni

--- src/cnnClassifier/pipeline/prediction.py
+++ src/cnnClassifier/pipeline/prediction.py
@@ -3,46 +3,31 @@ import numpy as np
 import tensorflow as tf
 from keras.utils import load_img, img_to_array
 
-
 class PredictionPipeline:
     def __init__(self, filename):
         self.filename = filename
+        
+        self.model_path = os.path.join("artifacts", "training", "model.keras")
 
     def predict(self):
+        
+        if not os.path.exists(self.model_path):
+            return f"Error: Model file not found at {self.model_path}"
 
-        # DEBUGGING START
-        print("Current Working Directory:", os.getcwd())
+        
+        model = tf.keras.models.load_model(self.model_path)
 
-        model_path = os.path.join(
-            "artifacts",
-            "training",
-            "model.keras"
-        )
-
-        print("Model Path:", model_path)
-        print("Model Exists:", os.path.exists(model_path))
-
-        print("Uploaded File:", self.filename)
-        print("File Exists:", os.path.exists(self.filename))
-        # DEBUGGING END
-
-        model = tf.keras.models.load_model(model_path)
-
-        test_image = load_img(
-            self.filename,
-            target_size=(224, 224)
-        )
-
+        test_image = load_img(self.filename, target_size=(224, 224))
         test_image = img_to_array(test_image)
-
-        test_image = test_image / 255.0
-
+        
+        
+        test_image = test_image.astype('float32') / 255.0
         test_image = np.expand_dims(test_image, axis=0)
 
         result = model.predict(test_image)
 
-        print("Prediction:", result)
-        print("Shape:", result.shape)
+        
+        tf.keras.backend.clear_session()
 
         if result[0][0] > 0.5:
             prediction = "normal"
